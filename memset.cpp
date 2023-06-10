73cc8be5e27a0059fadaf5c068afc07c32f88b56
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2023-06-10 14:52:12 +0530

    memset.cpp

---
+++ memset.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+    int arr[10];
+    
+    memset(arr,0,sizeof(arr));
+    
+    for(int i=0;i<10;i++){
+        cout<<arr[i]<< " ";
+    }
+
+    return 0;
+}
